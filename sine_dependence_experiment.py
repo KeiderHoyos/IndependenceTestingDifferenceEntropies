ee7eb18375c972010d023b899c9e4c3e8645a633
KeiderHoyos <keider.hoyos@uky.edu>
2024-09-05 15:38:28 -0400

    removing seaborn

--- sine_dependence_experiment.py
+++ sine_dependence_experiment.py
@@ -7,7 +7,6 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
-import seaborn as sns
 import scipy.stats as stats
 from scipy.stats import gamma
 from scipy.stats import ortho_group
